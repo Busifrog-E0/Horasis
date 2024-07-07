e795982d7d6078bc6191da642bf1cc1483a760ba
Haris Wilson <harismoylan@gmail.com>
2024-07-07 22:52:44 +0530

    AccountVerificationEmail parameter changes

--- server/controllers/emails-controller.js
+++ server/controllers/emails-controller.js
@@ -2,12 +2,16 @@ import { EmailAPI } from "./zeptomail-controller"
 
 /**
  * @param {import('./../databaseControllers/users-databaseController.js').UserData} UserData 
- * @param {{"name":string,"verification_link":string,"username":string}} MergeInfo 
+ * @param {string} verification_link 
  * @returns {Promise<string|true>}
  */
-const AccountVerificationEmail = async (UserData, MergeInfo) => {
-    // @ts-ignore
-    MergeInfo = { ...MergeInfo, "team": "Horissa", "product_name": "Horissa" };
+const AccountVerificationEmail = async (UserData, verification_link) => {
+    const MergeInfo = {
+        verification_link,
+        name: UserData.FullName,
+        username: UserData.Username,
+        team: "Horissa", product_name: "Horissa",
+    };
     const VerificationTemplateId = "2518b.7d5b154c30adb8bd.k1.834092b0-3b59-11ef-b3cf-525400674725.190868a075b";
     return EmailAPI(UserData.Email, "Verification Link", VerificationTemplateId, MergeInfo);
 }
