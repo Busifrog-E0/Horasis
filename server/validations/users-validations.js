2a51f35ce32f01a9d73f13d5f386e62d3987b743
vedanthp16 <pvedanth2@gmail.com>
2024-07-08 16:05:25 +0530

    username added

--- server/validations/users-validations.js
+++ server/validations/users-validations.js
@@ -3,6 +3,7 @@ import { QueryParametersSchema } from './common.js';
 
 const UserSchema = Joi.object({
     FullName: Joi.string().required(),
+    Username: Joi.string().required(),
     Email: Joi.string().email().required(),
     Password: Joi.string().min(8).required(),
     Country: Joi.string().required(),
