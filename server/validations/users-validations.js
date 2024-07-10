35b8635feb195cc30ac7a834746126a96c290cdc
vedanthp16 <pvedanth2@gmail.com>
2024-07-10 13:59:18 +0530

    fixes

--- server/validations/users-validations.js
+++ server/validations/users-validations.js
@@ -96,9 +96,24 @@ const ValidatePatchUsers = async (req, res, next) => {
 
 }
 
+const ValidatePatchUserPictures = async (req, res, next) => {
+    const Result = Joi.object({
+        CoverPicture: Joi.string().uri().optional(),
+        ProfilePicture: Joi.string().uri().optional(),
+    }).xor('CoverPicture', 'ProfilePicture').validate(req.body, { stripUnknown: true });
+    if (Result.error) {
+        const message = Result.error.details.map((detail) => detail.message).join(', ');
+        return res.status(400).json(message);
+    }
+    else {
+        req.body = Result.value;
+        return next();
+    }
+}
+
 
 export {
     ValidateUserRegister, ValidateUserLogin,
     ValidateVerifyOTP, ValidateCheckUsername,
-    ValidatePatchUsers
+    ValidatePatchUsers,ValidatePatchUserPictures
 }
