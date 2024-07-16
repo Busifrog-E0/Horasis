cb869e41d9a5ff055425413029fafa131cb07c4d
Karthik M S <k2g1001@gmail.com>
2024-07-16 14:40:16 +0530

    connections in userprofile

--- horasis-frontend/src/pages/MyProfile.jsx
+++ horasis-frontend/src/pages/MyProfile.jsx
@@ -21,6 +21,8 @@ import cover from '../assets/icons/cover.svg'
 import EmptyMembers from '../components/Common/EmptyMembers'
 import ConnectionsTab from '../components/Connections/Tabs/ConnectionsTab'
 import { useToast } from '../components/Toast/ToastService'
+import { getNextId } from '../utils/URLParams'
+import { jsonToQuery } from '../utils/searchParams/extractSearchParams'
 
 const tabs = (user, getUserDetails) => [
 	{
@@ -123,11 +125,92 @@ const tabs = (user, getUserDetails) => [
 	{
 		key: 2,
 		title: 'Connections',
-		render: () => (
-			<div className='bg-system-secondary-bg p-4 lg:p-6 rounded-b-lg '>
-				<ConnectionsTab />
-			</div>
-		),
+		render: () => {
+			const { updateCurrentUser, currentUserData } = useContext(AuthContext)
+			const toast = useToast()
+			const [connections, setConnections] = useState([])
+			const [isLoading, setIsLoading] = useState(true)
+			const [isLoadingMore, setIsLoadingMore] = useState(false)
+			const [pageDisabled, setPageDisabled] = useState(true)
+			const [filters, setFilters] = useState({
+				OrderBy: 'Index',
+				Keyword: '',
+				Limit: 10,
+				Keyword: '',
+			})
+			const setLoadingCom = (tempArr, value) => {
+				if (tempArr.length > 0) {
+					setIsLoadingMore(value)
+				} else {
+					setIsLoading(value)
+				}
+			}
+			const getConnections = (tempConnections) => {
+				setLoadingCom(tempConnections, true)
+				getItem(
+					`users/${currentUserData.CurrentUser.UserId}/connections?&NextId=${getNextId(tempConnections)}&${jsonToQuery(
+						filters
+					)}`,
+					(data) => {
+						setConnections([...tempConnections, ...data])
+						setLoadingCom(tempConnections, false)
+					},
+					(err) => {
+						setLoadingCom(tempConnections, false)
+						// console.log(err)
+					},
+					updateCurrentUser,
+					currentUserData,
+					toast
+				)
+			}
+
+			const hasAnyLeft = () => {
+				getItem(
+					`users/${currentUserData.CurrentUser.UserId}/connections?NextId=${getNextId(connections)}&${jsonToQuery({
+						...filters,
+						Limit: 1,
+					})}`,
+					(data) => {
+						if (data?.length > 0) {
+							setPageDisabled(false)
+						} else {
+							setPageDisabled(true)
+						}
+					},
+					(err) => {
+						setPageDisabled(true)
+					},
+					updateCurrentUser,
+					currentUserData,
+					toast
+				)
+			}
+
+			useEffect(() => {
+				getConnections([])
+			}, [])
+			useEffect(() => {
+				hasAnyLeft()
+			}, [connections])
+			return (
+				<div className='bg-system-secondary-bg p-4 lg:p-6 rounded-b-lg '>
+					<ConnectionsTab
+						getConnectionCount={() => {}}
+						data={connections}
+						getAllData={getConnections}
+						isLoading={isLoading}
+						setData={setConnections}
+						setIsLoading={setIsLoading}
+						fetchMore={() => {
+							getConnections(connections)
+						}}
+						isLoadingMore={isLoadingMore}
+						pageDisabled={pageDisabled}
+					/>
+				</div>
+			)
+		},
 	},
 	// {
 	//   key: 3,
