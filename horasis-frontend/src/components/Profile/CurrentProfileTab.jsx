f8f9e296155fcc04d1b2aee7709f1ffc68c9c283
Rajath.K.S <rajathkizhiyedam@gmail.com>
2024-07-18 20:25:42 +0530

    user && user?.ProfileCompletionPercentage != 100 && complete your profile shown

--- horasis-frontend/src/components/Profile/CurrentProfileTab.jsx
+++ horasis-frontend/src/components/Profile/CurrentProfileTab.jsx
@@ -13,7 +13,7 @@ const CurrentProfileTab = () => {
 	const [expand, setExpand] = useState(false)
 	const [isLoading, setIsLoading] = useState(false)
 	const [user, setUser] = useState()
-	const {followCount,getFollowCount} = useFollow()
+	const { followCount, getFollowCount } = useFollow()
 
 	const navigate = useNavigate()
 	const GoToProfilePage = () => {
@@ -46,11 +46,11 @@ const CurrentProfileTab = () => {
 				// console.log(err)
 			},
 			updateCurrentUser,
-			currentUserData,toast
+			currentUserData, toast
 		)
 	}
 
-	
+
 
 	useEffect(() => {
 		getUserDetails()
@@ -115,89 +115,82 @@ const CurrentProfileTab = () => {
 								{followCount && followCount.NoOfFollowers} Followers
 							</h4>
 						</div>
-						<div className='p-3 pb-2 px-5 bg-system-secondary-bg rounded-lg shadow-lg'>
-							<div
-								className='flex justify-between items-center mt-2 gap-3 cursor-pointer'
-								onClick={() => {
-									setExpand(!expand)
-								}}>
-								<h4 className='font-semibold text-sm text-center text-system-primary-accent mt-2'>
-									Complete Your Profile
-								</h4>
-								<h4 className='font-semibold text-base text-center text-system-primary-accent mt-2'>
-									{user && user.ProfileCompletionPercentage && user.ProfileCompletionPercentage}%
-								</h4>
-							</div>
-							<div className='w-full bg-system-secondary-selected-accent rounded-full h-2 mt-3 mb-1'>
+						{user && user?.ProfileCompletionPercentage != 100 &&
+							<div className='p-3 pb-2 px-5 bg-system-secondary-bg rounded-lg shadow-lg'>
 								<div
-									className='bg-brand-green h-2 rounded-full'
-									style={{
-										width: `${
-											user?.ProfileCompletionPercentage ? user.ProfileCompletionPercentage : '0'
-										}%`,
-									}}></div>
-							</div>
-							{expand && (
-								<>
-									<div className='pt-2'>
-										<div className='relative '>
-											<div className='flex flex-row items-center gap-1'>
-												<div
-													className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${
-														user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
+									className='flex justify-between items-center mt-2 gap-3 cursor-pointer'
+									onClick={() => {
+										setExpand(!expand)
+									}}>
+									<h4 className='font-semibold text-sm text-center text-system-primary-accent mt-2'>
+										Complete Your Profile
+									</h4>
+									<h4 className='font-semibold text-base text-center text-system-primary-accent mt-2'>
+										{user && user.ProfileCompletionPercentage && user.ProfileCompletionPercentage}%
+									</h4>
+								</div>
+								<div className='w-full bg-system-secondary-selected-accent rounded-full h-2 mt-3 mb-1'>
+									<div
+										className='bg-brand-green h-2 rounded-full'
+										style={{
+											width: `${user?.ProfileCompletionPercentage ? user.ProfileCompletionPercentage : '0'
+												}%`,
+										}}></div>
+								</div>
+								{expand && (
+									<>
+										<div className='pt-2'>
+											<div className='relative '>
+												<div className='flex flex-row items-center gap-1'>
+													<div
+														className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
 															? 'brand-gray-dim'
 															: 'brand-green'
-													} text-${
-														user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
-															? 'brand-gray-dim'
-															: 'white'
-													}`}>
-													{user.JobTitle === '' || user.CompanyName === '' || user.Industry === '' ? (
-														<></>
-													) : (
-														<span>✓</span>
-													)}
-												</div>
-												<p
-													className={`text-sm font-medium text-${
-														user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
+															} text-${user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
+																? 'brand-gray-dim'
+																: 'white'
+															}`}>
+														{user.JobTitle === '' || user.CompanyName === '' || user.Industry === '' ? (
+															<></>
+														) : (
+															<span>✓</span>
+														)}
+													</div>
+													<p
+														className={`text-sm font-medium text-${user.JobTitle === '' || user.CompanyName === '' || user.Industry === ''
 															? 'brand-gray-dim'
 															: 'brand-green'
-													} text-system-primary-text`}>
-													Work Information
-												</p>
-											</div>
-											<div className=' pt-3 '></div>
-											<div className='flex flex-row items-center gap-1'>
-												<div
-													className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${
-														user.ProfilePicture !== '' ? 'brand-green' : 'brand-gray-dim'
-													} text-${user.ProfilePicture !== '' ? 'white' : 'brand-gray-dim'}`}>
-													{user.ProfilePicture !== '' && <span>✓</span>}
+															} text-system-primary-text`}>
+														Work Information
+													</p>
 												</div>
-												<p
-													className={`text-sm font-medium text-${
-														user.ProfilePicture !== '' ? 'brand-green' : 'brand-gray-dim'
-													} text-system-primary-text`}>
-													Profile Photo
-												</p>
-											</div>
-											<div className='pt-3 '></div>
-											<div className='flex flex-row items-center gap-1'>
-												<div
-													className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${
-														user.About !== '' ? 'brand-green' : 'brand-gray-dim'
-													} text-${user.About !== '' ? 'white' : 'brand-gray-dim'}`}>
-													{user.About !== '' && <span>✓</span>}
+												<div className=' pt-3 '></div>
+												<div className='flex flex-row items-center gap-1'>
+													<div
+														className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${user.ProfilePicture !== '' ? 'brand-green' : 'brand-gray-dim'
+															} text-${user.ProfilePicture !== '' ? 'white' : 'brand-gray-dim'}`}>
+														{user.ProfilePicture !== '' && <span>✓</span>}
+													</div>
+													<p
+														className={`text-sm font-medium text-${user.ProfilePicture !== '' ? 'brand-green' : 'brand-gray-dim'
+															} text-system-primary-text`}>
+														Profile Photo
+													</p>
 												</div>
-												<p
-													className={`text-sm font-medium text-${
-														user.About !== '' ? 'brand-green' : 'brand-gray-dim'
-													} text-system-primary-text`}>
-													Biography
-												</p>
-											</div>
-											{/* {steps.map((event, index) => (
+												<div className='pt-3 '></div>
+												<div className='flex flex-row items-center gap-1'>
+													<div
+														className={`w-4 h-4 p-2 rounded-full flex items-center justify-center bg-${user.About !== '' ? 'brand-green' : 'brand-gray-dim'
+															} text-${user.About !== '' ? 'white' : 'brand-gray-dim'}`}>
+														{user.About !== '' && <span>✓</span>}
+													</div>
+													<p
+														className={`text-sm font-medium text-${user.About !== '' ? 'brand-green' : 'brand-gray-dim'
+															} text-system-primary-text`}>
+														Biography
+													</p>
+												</div>
+												{/* {steps.map((event, index) => (
 												<div key={index} className=''>
 													<div className='flex flex-row items-center gap-1'>
 														<div
@@ -218,18 +211,17 @@ const CurrentProfileTab = () => {
 													{index !== 2 && <div className='border-l-2 border-gray-200 pt-3 mx-1.5'></div>}
 												</div>
 											))} */}
+											</div>
 										</div>
-									</div>
-									{user && user?.ProfileCompletionPercentage != 100 && (
 										<h1
 											className='text-center cursor-pointer text-sm font-semibold underline text-system-primary-accent mt-2'
 											onClick={() => GoToProfilePage()}>
 											Complete Your Profile
 										</h1>
-									)}
-								</>
-							)}
-						</div>
+									</>
+								)}
+							</div>
+						}
 					</>
 				)}
 			</div>
