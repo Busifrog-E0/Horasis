29efb2f9818bcc3136e4140b525c4dd7c3e94e77
Karthik M S <k2g1001@gmail.com>
2024-07-25 10:00:08 +0530

    post with permissions

--- horasis-frontend/src/components/Activities/PostComponent.jsx
+++ horasis-frontend/src/components/Activities/PostComponent.jsx
@@ -346,7 +346,9 @@ const PostComponent = ({
 	useEffect(() => {
 		getUserDetails()
 	}, [])
-
+	if (!permissions.IsAdmin && !permissions.CanPostActivity) {
+		return <></>
+	}
 	return (
 		<div className={`bg-system-secondary-bg rounded-xl relative ${className}`}>
 			{isLoading && (
@@ -362,10 +364,7 @@ const PostComponent = ({
 					style={{ display: 'none' }}
 					ref={docFileInputRef}
 				/>
-				{(permissions.IsAdmin ||
-					permissions.CanCreateAlbum ||
-					permissions.CanUploadPhoto ||
-					permissions.CanUploadVideo) && (
+				{(permissions.CanCreateAlbum || permissions.CanUploadPhoto || permissions.CanUploadVideo) && (
 					<input
 						type='file'
 						accept='image/jpeg, image/png, video/mp4'
@@ -429,7 +428,7 @@ const PostComponent = ({
 									d='M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z'
 								/>
 							</svg>
-							{(fullPermissionAllowed || permissions.CanUploadPhoto) && (
+							{(permissions.CanCreateAlbum || permissions.CanUploadPhoto || permissions.CanUploadVideo) && (
 								<svg
 									onClick={handleImageUploadClick}
 									xmlns='http://www.w3.org/2000/svg'
