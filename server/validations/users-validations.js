e39ef6d48dc47541b3a0034438fc97892d2d63d9
vedanthp16 <pvedanth2@gmail.com>
2024-08-07 13:25:39 +0530

    forget password

--- server/validations/users-validations.js
+++ server/validations/users-validations.js
@@ -125,10 +125,41 @@ const ValidateGetUserMedia = async (req, res, next) => {
     }
 }
 
+const ValidatePostForgotPassword = async (req, res, next) => {
+    const Result = Joi.object({
+        Email: Joi.string().email().required(),
+    }).validate(req.body, { stripUnknown: true });
+    if (Result.error) {
+        const message = Result.error.details.map((detail) => detail.message).join(', ');
+        return res.status(400).json(message);
+    }
+    else {
+        req.body = Result.value;
+        return next();
+    }
+}
+
+const ValidatePasswordReset = async (req, res, next) => {
+    const Result = Joi.object({
+        Password: Joi.string().min(8).required(),
+        OTPId : Joi.string().required(),
+    }).validate(req.body, { stripUnknown: true });
+    if (Result.error) {
+        const message = Result.error.details.map((detail) => detail.message).join(', ');
+        return res.status(400).json(message);
+    }
+    else {
+        req.body = Result.value;
+        return next();
+    }
+}
+
 
 export {
     ValidateUserRegister, ValidateUserLogin,
     ValidateVerifyOTP, ValidateCheckUsername,
     ValidatePatchUsers, ValidatePatchUserPictures,
-    ValidateGetUserMedia
+    ValidateGetUserMedia, ValidatePostForgotPassword,
+    ValidatePasswordReset
+    
 }
