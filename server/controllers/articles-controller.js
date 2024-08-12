7a7f02383317f6383f4f2b2e153dcedb3d46503b
vedanthp16 <pvedanth2@gmail.com>
2024-08-13 00:27:08 +0530

    articles init

---
+++ server/controllers/articles-controller.js
@@ -0,0 +1,71 @@
+import e from 'express';
+
+import { ReadOneFromArticles, ReadArticles, UpdateArticles, CreateArticles, RemoveArticles, } from './../databaseControllers/articles-databaseController.js';
+/**
+ * @typedef {import('./../databaseControllers/articles-databaseController.js').ArticleData} ArticleData 
+ */
+
+/**
+ * 
+ * @param {e.Request} req 
+ * @param {e.Response} res 
+ * @returns {Promise<e.Response<ArticleData>>}
+ */
+const GetOneFromArticles = async (req, res) => {
+    const { ArticleId } = req.params;
+    const data = await ReadOneFromArticles(ArticleId);
+    return res.json(data);
+}
+
+/**
+ * 
+ * @param {e.Request} req 
+ * @param {e.Response} res 
+ * @returns {Promise<e.Response<Array<ArticleData>>>}
+ */
+const GetArticles = async (req, res) => {
+    const { Filter, NextId, Limit, OrderBy } = req.query;
+    // @ts-ignore
+    const data = await ReadArticles(Filter, NextId, Limit, OrderBy);
+    return res.json(data);
+}
+
+/**
+ * 
+ * @param {e.Request} req 
+ * @param {e.Response} res 
+ * @returns {Promise<e.Response<true>>}
+ */
+const PostArticles = async (req, res) => {
+    await CreateArticles(req.body);
+    return res.json(true);
+}
+
+/**
+ * 
+ * @param {e.Request} req 
+ * @param {e.Response} res 
+ * @returns {Promise<e.Response<true>>}
+ */
+const PatchArticles = async (req, res) => {
+    const { ArticleId } = req.params;
+    await UpdateArticles(req.body, ArticleId);
+    return res.json(true);
+}
+
+/**
+ * 
+ * @param {e.Request} req 
+ * @param {e.Response} res 
+ * @returns {Promise<e.Response<true>>}
+ */
+const DeleteArticles = async (req, res) => {
+    const { ArticleId } = req.params;
+    await RemoveArticles(ArticleId);
+    return res.json(true);
+}
+
+
+export {
+    GetOneFromArticles, GetArticles, PostArticles, PatchArticles, DeleteArticles
+}
---
+++ server/databaseControllers/articles-databaseController.js
@@ -0,0 +1,79 @@
+import dataHandling from './functions.js'
+
+/**
+ * @typedef {import('./users-databaseController.js').UserData} UserData
+ */
+
+/**
+ * @typedef {object} ArticleData
+ * @property {string} ArticleName
+ * @property {string} Description
+ * @property {string} DocId
+ * @property {string} CoverPhoto
+ * @property {number} CreatedIndex
+ * @property {string} AuthorId
+ * @property {UserData} UserDetails
+ * 
+ */
+
+
+
+/**
+ * 
+ * @param {undefined|object} Where 
+ * @param {undefined|string} NextIndex 
+ * @param {undefined|number} Limit 
+ * @param {undefined|object} orderBy 
+ * @returns {Promise<Array<ArticleData>>} Returns ArticleData
+ */
+const ReadArticles = async (Where, NextIndex, Limit, orderBy) => {
+    return dataHandling.Read('Articles', undefined, NextIndex, Limit, Where, orderBy);
+}
+
+/**
+ * 
+ * @param {string} DocId 
+ * @returns {Promise<ArticleData>}
+ */
+const ReadOneFromArticles = async (DocId) => {
+    return dataHandling.Read('Articles', DocId);
+}
+
+/**
+ * 
+ * @param {ArticleData|object} data
+ * @param {string} DocId 
+ * @returns {Promise<boolean>}
+ */
+const UpdateArticles = async (data, DocId) => {
+    return dataHandling.Update('Articles', data, DocId);
+}
+
+
+/**
+ * 
+ * @param {ArticleData|object} data
+ * @param {string|undefined} DocId 
+ * @returns {Promise<string>}
+ */
+const CreateArticles = async (data, DocId = undefined) => {
+    return dataHandling.Create('Articles', data, DocId);
+}
+
+/**
+ * 
+ * @param {string} DocId 
+ * @returns {Promise<boolean>}
+ */
+const RemoveArticles = async (DocId) => {
+    return dataHandling.Delete('Articles', DocId);
+}
+
+
+export {
+    ReadArticles,
+    ReadOneFromArticles,
+    UpdateArticles,
+    CreateArticles,
+    RemoveArticles
+}
