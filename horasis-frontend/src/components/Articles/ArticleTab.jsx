e693016c56c9df243dd32a783d1487cf2432c7dc
Karthik M S <k2g1001@gmail.com>
2024-08-16 11:26:19 +0530

    article coverphoto change

--- horasis-frontend/src/components/Articles/ArticleTab.jsx
+++ horasis-frontend/src/components/Articles/ArticleTab.jsx
@@ -3,11 +3,11 @@ import DropdownMenu from '../ui/DropdownMenu'
 
 const ArticleTab = ({ article, navigateToArticle }) => {
 	return (
-		<div className='p-5 bg-system-secondary-bg rounded-lg cursor-pointer' onClick={() => navigateToArticle(article.DocId)}>
-			<div className='h-52 overflow-hidden rounded-lg'>
+		<div className='p-0 bg-system-secondary-bg rounded-lg cursor-pointer' onClick={() => navigateToArticle(article.DocId)}>
+			<div className='h-52 overflow-hidden rounded-t-lg'>
 				<img src={article.CoverPhoto} className='object-cover h-full w-full' />
 			</div>
-			<div className='px-2'>
+			<div className='px-5 pb-5'>
 				<h4 className='font-semibold text-lg text-system-primary-accent mt-2'>{article.ArticleName}</h4>
 				<h4 className='text-xs text-system-primary-accent'>by {article.UserDetails.FullName}</h4>
 				<h4 className='text-sm text-brand-gray-dim mt-2'>{relativeTime(article.CreatedIndex)}</h4>
--- horasis-frontend/src/pages/SingleArticles.jsx
+++ horasis-frontend/src/pages/SingleArticles.jsx
@@ -1,10 +1,14 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import cover from '../assets/icons/cover.svg'
 import { useEffect, useState } from 'react'
-import { getItem } from '../constants/operations'
+import { getItem, patchItem, postItem } from '../constants/operations'
 import { useAuth } from '../utils/AuthProvider'
 import { useToast } from '../components/Toast/ToastService'
 import arrowback from '../assets/icons/arrowback.svg'
+import camera from '../assets/icons/camera.svg'
+import { getDateInWordsFormat } from '../utils/date'
+import Modal from '../components/ui/Modal'
+import PictureUpload from '../components/Profile/EditProfile/PictureUpload'
 
 const SingleArticles = () => {
 	const { updateCurrentUser, currentUserData } = useAuth()
@@ -37,43 +41,152 @@ const SingleArticles = () => {
 	useEffect(() => {
 		getArticle()
 	}, [])
-	return (
-		<div  className='p-4 md:px-10 max-w-9xl md:py-10'>
-			<div className='overflow-hidden h-80 lg:h-96 relative rounded-t-2xl'>
-				{article.CoverPhoto ? (
-					<>
-						<img src={article.CoverPhoto} className='object-cover h-full w-full' />
-					</>
-				) : (
-					<>
-						<img src={cover} className='object-cover h-full w-full' />
-					</>
-				)}
-				<div className='absolute top-0 right-0 left-0 bottom-0 flex flex-col justify-between items-start p-4 lg:px-10 lg:py-6  h-100 overflow-hidden overflow-y-auto'>
-					<div className='flex w-full items-start justify-between'>
-						<div
-							className={`inline-flex items-center justify-center w-12 h-12 p-3 overflow-hidden rounded-full border border-white bg-white cursor-pointer`}
-							onClick={handleGoBack}>
-							<img src={arrowback} alt='' className='h-6 cursor-pointer' />
 
-							{/* <h4 className='font-medium text-xl text-brand-secondary'>Back</h4> */}
-						</div>
+	// cover photo upload logic
+	const [isImageUploading, setIsImageUploading] = useState(false)
+	const [selectedCoverImage, setSelectedCoverImage] = useState(null)
+	const [coverImageToUpload, setCoverImageToUpload] = useState(null)
+	const [isCoverPictureOpen, setIsCoverPictureOpen] = useState(false)
+	const onCoverImageSelect = (imageData) => {
+		setCoverImageToUpload(imageData)
+		const tempUrl = URL.createObjectURL(new Blob([new Uint8Array(imageData.FileData)]))
+		setSelectedCoverImage(tempUrl)
+	}
+	const onCoverImageDelete = () => {
+		setCoverImageToUpload(null)
+		setSelectedCoverImage(null)
+	}
+	const onCoverImageSet = (url) => {
+		setIsImageUploading(true)
+
+		patchItem(
+			`articles/${articleid}/coverPicture`,
+			{
+				CoverPhoto: url,
+			},
+			(result) => {
+				if (result === true) {
+					setIsCoverPictureOpen(false)
+					getDiscussion()
+				}
+				setIsImageUploading(false)
+			},
+			(err) => {
+				// console.log(err)
+				setIsImageUploading(false)
+			},
+			updateCurrentUser,
+			currentUserData,
+			toast
+		)
+	}
+	const onCoverImageUpload = () => {
+		if (coverImageToUpload) {
+			setIsImageUploading(true)
+
+			postItem(
+				'files/users',
+				coverImageToUpload,
+				(result) => {
+					onCoverImageSet(result.FileUrl)
+				},
+				(err) => {
+					// console.log(err)
+					setIsImageUploading(false)
+				},
+				updateCurrentUser,
+				currentUserData,
+				toast
+			)
+		} else {
+			onCoverImageSet('')
+		}
+	}
+
+	return (
+		<>
+			<Modal isOpen={isCoverPictureOpen} maxWidth='max-w-4xl'>
+				<Modal.Header>
+					<div className='p-2 flex items-center justify-between w-full'>
+						<p className='text-lg font-medium'>Cover Photo</p>
+						<button
+							onClick={() => {
+								setIsCoverPictureOpen(false)
+							}}
+							disabled={isImageUploading}>
+							<img src={close} className='h-6  cursor-pointer' alt='' />
+						</button>
 					</div>
-					<div>
-						<h4 className='font-medium shadow-lg text-4xl text-white mb-3'>{article.ArticleName}</h4>
-						<div className='flex flex-row flex-wrap gap-3'>
-							{/* <h4 className='text-2xl text-white'>Virtual Event</h4>
-							<h4 className='text-2xl text-white'>•</h4> */}
+				</Modal.Header>
+				<Modal.Body>
+					<p className='text-lg font-medium text-center'>
+						Your cover photo will be used to customize the header of your profile.
+					</p>
+					<div className=' flex flex-col items-center justify-center pt-10'>
+						<PictureUpload
+							// isBanner={false}
+							altTitle='Cover Picture'
+							selectedImage={selectedCoverImage}
+							setSelectedImage={setSelectedCoverImage}
+							onImageSelect={onCoverImageSelect}
+							onImageDelete={onCoverImageDelete}
+							onUploadImage={onCoverImageUpload}
+							fileFieldName={'CoverPicture'}
+							isUploading={isImageUploading}
+						/>
+					</div>
+				</Modal.Body>
+			</Modal>
+			<div className='p-4 md:px-10 max-w-9xl md:py-10'>
+				<div className='overflow-hidden h-80 lg:h-96 relative rounded-t-2xl'>
+					{article.CoverPhoto ? (
+						<>
+							<img src={article.CoverPhoto} className='object-cover h-full w-full' />
+						</>
+					) : (
+						<>
+							<img src={cover} className='object-cover h-full w-full' />
+						</>
+					)}
+					<div className='absolute top-0 right-0 left-0 bottom-0 flex flex-col justify-between items-start p-4 lg:px-10 lg:py-6  h-100 overflow-hidden overflow-y-auto'>
+						<div className='flex w-full items-start justify-between'>
+							<div
+								className={`inline-flex items-center justify-center w-12 h-12 p-3 overflow-hidden rounded-full border border-white bg-white cursor-pointer`}
+								onClick={handleGoBack}>
+								<img src={arrowback} alt='' className='h-6 cursor-pointer' />
+
+								{/* <h4 className='font-medium text-xl text-brand-secondary'>Back</h4> */}
+							</div>
+							{article?.UserDetails?.DocId === currentUserData.CurrentUser.UserId && (
+								<div
+									onClick={() => {
+										setIsCoverPictureOpen(true)
+										if (article.CoverPhoto) {
+											setSelectedCoverImage(article.CoverPhoto)
+										} else {
+											setSelectedCoverImage(null)
+										}
+									}}
+									className={`inline-flex items-center justify-center w-12 h-12 p-3 overflow-hidden rounded-full border border-white bg-white cursor-pointer`}>
+									<img src={camera} alt='' className='h-6 cursor-pointer' />
+								</div>
+							)}
+						</div>
+						<div>
+							<h4 className='font-medium shadow-lg text-4xl text-white mb-3'>{article.ArticleName}</h4>
+							<div className='flex flex-row flex-wrap gap-3'>
+								<h4 className='text-2xl text-white'>{article?.UserDetails?.FullName}</h4>
+								<h4 className='text-2xl text-white'>•</h4>
+								<h4 className='text-2xl text-white'>{getDateInWordsFormat(new Date(article?.CreatedIndex))}</h4>
+							</div>
 						</div>
 					</div>
 				</div>
+				<div className='lg:col-span-3 px-10 bg-system-secondary-bg py-10 rounded-b-2xl'>
+					<h4 className='text-xl text-brand-gray  leading-8 whitespace-pre-line'>{article.Description}</h4>
+				</div>
 			</div>
-			<div className='lg:col-span-3 px-10 bg-system-secondary-bg py-10 rounded-b-2xl'>
-				<h4 className='text-xl text-brand-gray  leading-8 whitespace-pre-line'>
-					{article.Description}
-				</h4>
-			</div>
-		</div>
+		</>
 	)
 }
 
