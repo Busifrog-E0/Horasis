1d7432cc3121241e68719a8e66b8e1a507096fe6
Karthik M S <k2g1001@gmail.com>
2024-08-20 17:20:41 +0530

    hide profile tab in mobile in certain routes

--- horasis-frontend/src/layouts/ProfileTabLayout.jsx
+++ horasis-frontend/src/layouts/ProfileTabLayout.jsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import CurrentProfileTab from '../components/Profile/CurrentProfileTab'
 import TodaysEventTab from '../components/Events/TodaysEventTab'
 import RecentlyActiveMemebrsTab from '../components/Members/RecentlyActiveMemebrsTab'
+import { useEffect } from 'react'
 
 const ProfileTabLayout = () => {
+	const location = useLocation()
+	const checkHiddenInMobile = () => {
+		const arr = ['Connections', 'Articles', 'Discussions']
+		let hide = arr.some((item) => location.pathname.includes(item))
+		return hide
+	}
 	return (
 		<div className='p-2 lg:px-10 lg:py-6'>
 			<div className='grid lg:grid-cols-4 gap-3 lg:gap-12'>
-				<div className='sticky h-max top-0'>
+				<div className={`${checkHiddenInMobile() ? 'hidden lg:block' : ''} lg:sticky h-max top-0`}>
 					<CurrentProfileTab />
 					<h4 className='font-medium text-xl text-system-primary-text mt-3 lg:mt-5'>Today's Event</h4>
 					<TodaysEventTab />
