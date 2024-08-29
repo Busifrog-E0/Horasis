ca3dea56a524140e953433a18cf3aa042330a86a
Karthik M S <k2g1001@gmail.com>
2024-08-29 16:06:47 +0530

    recently active members

--- horasis-frontend/src/components/Members/RecentlyActiveMemebrsTab.jsx
+++ horasis-frontend/src/components/Members/RecentlyActiveMemebrsTab.jsx
@@ -1,110 +1,117 @@
-import { useContext, useEffect, useState } from "react"
-import { AuthContext } from "../../utils/AuthProvider"
-import { useToast } from "../Toast/ToastService"
-import { getNextId } from "../../utils/URLParams"
-import { jsonToQuery } from "../../utils/searchParams/extractSearchParams"
-import { getItem } from "../../constants/operations"
-import Spinner from "../ui/Spinner"
+import { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../utils/AuthProvider'
+import { useToast } from '../Toast/ToastService'
+import { getNextId } from '../../utils/URLParams'
+import { jsonToQuery } from '../../utils/searchParams/extractSearchParams'
+import { getItem } from '../../constants/operations'
+import Spinner from '../ui/Spinner'
 import avatar from '../../assets/icons/avatar.svg'
+import { useNavigate } from 'react-router-dom'
 
 const RecentlyActiveMemebrsTab = () => {
-    const { updateCurrentUser, currentUserData } = useContext(AuthContext)
-    const toast = useToast()
-    const [members, setMembers] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [isLoadingMore, setIsLoadingMore] = useState(false)
-    const [pageDisabled, setPageDisabled] = useState(true)
-    const [filters, setFilters] = useState({
-        OrderBy: 'Index',
-        Limit: 10,
-        Keyword: '',
-    })
-    const setLoadingCom = (tempArr, value) => {
-        if (tempArr.length > 0) {
-            setIsLoadingMore(value)
-        } else {
-            setIsLoading(value)
-        }
-    }
-    const getMembers = (tempMembers) => {
-        setLoadingCom(tempMembers, true)
-        getItem(
-            `users?NextId=${getNextId(tempMembers)}&${jsonToQuery(
-                filters
-            )}`,
-            (data) => {
-                setMembers([...tempMembers, ...data])
-                setLoadingCom(tempMembers, false)
-            },
-            (err) => {
-                setLoadingCom(tempMembers, false)
-                // console.log(err)
-            },
-            updateCurrentUser,
-            currentUserData,
-            toast
-        )
-    }
+	const { updateCurrentUser, currentUserData } = useContext(AuthContext)
+	const toast = useToast()
+	const [members, setMembers] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
+	const [isLoadingMore, setIsLoadingMore] = useState(false)
+	const [pageDisabled, setPageDisabled] = useState(true)
+	const [filters, setFilters] = useState({
+		OrderBy: ['Online','LastActive'],
+		Limit: 10,
+		Keyword: '',
+	})
+	const setLoadingCom = (tempArr, value) => {
+		if (tempArr.length > 0) {
+			setIsLoadingMore(value)
+		} else {
+			setIsLoading(value)
+		}
+	}
+	const getMembers = (tempMembers) => {
+		setLoadingCom(tempMembers, true)
+		getItem(
+			`users?NextId=${getNextId(tempMembers)}&${jsonToQuery(filters)}`,
+			(data) => {
+				setMembers([...tempMembers, ...data])
+				setLoadingCom(tempMembers, false)
+			},
+			(err) => {
+				setLoadingCom(tempMembers, false)
+				// console.log(err)
+			},
+			updateCurrentUser,
+			currentUserData,
+			toast
+		)
+	}
 
-    const hasAnyLeft = () => {
-        getItem(
-            `users?NextId=${getNextId(members)}&${jsonToQuery({
-                ...filters,
-                Limit: 1,
-            })}`,
-            (data) => {
-                if (data?.length > 0) {
-                    setPageDisabled(false)
-                } else {
-                    setPageDisabled(true)
-                }
-            },
-            (err) => {
-                setPageDisabled(true)
-            },
-            updateCurrentUser,
-            currentUserData,
-            toast
-        )
-    }
+	const hasAnyLeft = () => {
+		getItem(
+			`users?NextId=${getNextId(members)}&${jsonToQuery({
+				...filters,
+				Limit: 1,
+			})}`,
+			(data) => {
+				if (data?.length > 0) {
+					setPageDisabled(false)
+				} else {
+					setPageDisabled(true)
+				}
+			},
+			(err) => {
+				setPageDisabled(true)
+			},
+			updateCurrentUser,
+			currentUserData,
+			toast
+		)
+	}
 
+	useEffect(() => {
+		getMembers([])
+	}, [])
+	useEffect(() => {
+		if (members.length > 0) hasAnyLeft()
+	}, [members])
+	// <img key={member.DocId}  src={member.ProfilePicture} alt="Rounded avatar" />
+	const navigate = useNavigate()
 
-    useEffect(() => {
-        getMembers([])
-    }, [])
-    useEffect(() => {
-        if (members.length > 0) hasAnyLeft()
-    }, [members])
-    // <img key={member.DocId}  src={member.ProfilePicture} alt="Rounded avatar" />
-
-    return (<>
-        {
-            isLoading ?
-                <Spinner />
-                :
-                <div className="pr-16">
-
-                    <div className="flex items-center flex-wrap gap-3">
-                        {members.map((member, index) => {
-                            return <div className="cursor-pointer" onClick={() => { }} key={member.DocId}>
-                                {member?.ProfilePicture ? (
-                                    <>
-                                        <img className="w-12 h-12 rounded-full object-cover" src={member?.ProfilePicture} alt='Rounded avatar' />
-                                    </>
-                                ) : (
-                                    <>
-                                        <img className="w-12 h-12 rounded-full object-cover" src={avatar} alt='Rounded avatar' />
-                                    </>
-                                )}
-                            </div>
-                        })
-
-                        }
-                    </div>
-                </div>
-        }
-
-    </>)
+	return (
+		<>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<div className='pr-16'>
+					<div className='flex items-center flex-wrap gap-3'>
+						{members.map((member, index) => {
+							if (member.DocId === currentUserData.CurrentUser.UserId) return
+							return (
+								<div
+									className='cursor-pointer relative'
+									onClick={() => navigate(`/ViewProfile/${member.DocId}`)}
+									key={member.DocId}>
+									{member.Online && <div className='bg-system-success h-3 w-3 rounded-md absolute top-0 right-0'></div>}
+									{member?.ProfilePicture ? (
+										<>
+											<img
+												className='w-12 h-12 rounded-full object-cover'
+												src={member?.ProfilePicture}
+												alt='Rounded avatar'
+											/>
+										</>
+									) : (
+										<>
+											<img className='w-12 h-12 rounded-full object-cover' src={avatar} alt='Rounded avatar' />
+										</>
+									)}
+								</div>
+							)
+						})}
+					</div>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default RecentlyActiveMemebrsTab
--- horasis-frontend/src/utils/searchParams/extractSearchParams.js
+++ horasis-frontend/src/utils/searchParams/extractSearchParams.js
@@ -1,48 +1,61 @@
-export const jsonToQuery = (json) => {
-    let result = Object.fromEntries(
-        Object.entries(json).map(([key, value]) => [
-            key === 'StartDate' ? 'Index[$gte]' : key === 'EndDate' ? 'Index[$lte]' : key,
-            value
-        ]).filter(([_, value]) => value !== undefined)
-    );
+// export const jsonToQuery = (json) => {
+//     console.log(json)
+//     let result = Object.fromEntries(
+//         Object.entries(json).map(([key, value]) => [
+//             key === 'StartDate' ? 'Index[$gte]' : key === 'EndDate' ? 'Index[$lte]' : key,
+//             value
+//         ]).filter(([_, value]) => value !== undefined)
+//     );
 
-    return new URLSearchParams(result);
+//     return new URLSearchParams(result);
+// }
+
+export const jsonToQuery = (json) => {
+	console.log(json)
+
+	let result = Object.entries(json).flatMap(([key, value]) => {
+		if (Array.isArray(value)) {
+			return value.map((v) => [key, v])
+		} else if (value !== undefined && value !== '') {
+			return [[key === 'StartDate' ? 'Index[$gte]' : key === 'EndDate' ? 'Index[$lte]' : key, value]]
+		}
+		return []
+	})
+
+	return new URLSearchParams(result)
 }
 
 export function extractSearchParams(searchParams) {
+	const paramsObject = {}
 
-    const paramsObject = {};
+	for (const [key, value] of searchParams) {
+		if (paramsObject[key]) {
+			if (Array.isArray(paramsObject[key])) {
+				paramsObject[key].push(value)
+			} else {
+				paramsObject[key] = [paramsObject[key], value]
+			}
+		} else {
+			paramsObject[key] = value
+		}
+	}
 
-    for (const [key, value] of searchParams) {
-        if (paramsObject[key]) {
-            if (Array.isArray(paramsObject[key])) {
-                paramsObject[key].push(value);
-            } else {
-                paramsObject[key] = [paramsObject[key], value];
-            }
-        } else {
-            paramsObject[key] = value;
-        }
-    }
-
-    return paramsObject
-
+	return paramsObject
 }
 
-
 export const QueryToJson = (query) => {
-    const params = new URLSearchParams(query);
-    const obj = {};
-    for (const [key, value] of params) {
-        if (obj[key]) {
-            if (Array.isArray(obj[key])) {
-                obj[key].push(value);
-            } else {
-                obj[key] = [obj[key], value];
-            }
-        } else {
-            obj[key] = value;
-        }
-    }
-    return (obj);
+	const params = new URLSearchParams(query)
+	const obj = {}
+	for (const [key, value] of params) {
+		if (obj[key]) {
+			if (Array.isArray(obj[key])) {
+				obj[key].push(value)
+			} else {
+				obj[key] = [obj[key], value]
+			}
+		} else {
+			obj[key] = value
+		}
+	}
+	return obj
 }
