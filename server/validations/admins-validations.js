8fc3a8e9e4ab7e29956639b3521a7ad89875b1af
vedanthp16 <pvedanth2@gmail.com>
2024-09-13 11:09:16 +0530

    admin validation changed

--- server/validations/admins-validations.js
+++ server/validations/admins-validations.js
@@ -30,7 +30,21 @@ const ValidateGetUsersByRole = async (req, res, next) => {
     }
 }
 
-const ValidatePatchUserRoles = async (req, res, next) => {
+const ValidateAddAdmin = async (req, res, next) => {
+    const Result = Joi.object({
+        UserIds: Joi.array().items(Joi.string().required()).required(),
+    }).validate(req.body, { stripUnknown: true });
+    if (Result.error) {
+        const message = Result.error.details.map((detail) => detail.message).join(', ');
+        return res.status(400).json(message);
+    }
+    else {
+        req.body = Result.value;
+        return next();
+    }
+}
+
+const ValidateRemoveAdmin = async (req, res, next) => {
     const Result = Joi.object({
         UserId: Joi.string().required(),
     }).validate(req.body, { stripUnknown: true });
@@ -43,7 +57,6 @@ const ValidatePatchUserRoles = async (req, res, next) => {
         return next();
     }
 }
-
 export {
-    ValidateAdmin, ValidatePatchUserRoles, ValidateGetUsersByRole
+    ValidateAdmin, ValidateAddAdmin, ValidateGetUsersByRole, ValidateRemoveAdmin,
 }
