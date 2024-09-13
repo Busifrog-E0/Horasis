8d84d3c6d8489c358648adbd2e91ca33810bd8e9
vedanthp16 <pvedanth2@gmail.com>
2024-09-13 11:36:41 +0530

    admin fixes

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -318,7 +318,7 @@ const GetUsersByRole = async (req, res) => {
         ]
     }
     // @ts-ignore
-    Filter.Roles = Filter.Role === "User" ? ["User"] : ["User", "Admin"];
+    Filter.Roles = Filter.Role === "User" ? { $all: ["User"] } : { $all: ["User", "Admin"] };
     delete Filter.Role;
     // @ts-ignore
     const Users = await ReadUsers(Filter, NextId, Limit, OrderBy);
