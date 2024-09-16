3cbcc6ad5bc76360dd25d4d5bb797c77b30cc059
Karthik M S <k2g1001@gmail.com>
2024-09-16 18:20:56 +0530

    hover pie slice tooltip

--- horasis-frontend/src/components/Analytics/PieChart.jsx
+++ horasis-frontend/src/components/Analytics/PieChart.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const PieChart = ({ data, setHoveredPieSlice, hoveredPieSlice, cursorPosition, setCursorPosition }) => {
 	const total = data.reduce((acc, item) => acc + item.value, 0)
 	let cumulativeValue = 0
@@ -5,10 +7,19 @@ const PieChart = ({ data, setHoveredPieSlice, hoveredPieSlice, cursorPosition, s
 	const centerX = 0 // X-coordinate of the center of the SVG
 	const centerY = 0 // Y-coordinate of the center of the SVG
 
+	// Function to update the cursor position
+	const handleMouseMove = (e) => {
+		const { clientX, clientY } = e
+		setCursorPosition({ x: clientX, y: clientY })
+	}
+
 	return (
 		<>
-			
-			<svg viewBox='-20 -20 40 40' className='w-60 h-60 mx-auto'>
+			<svg
+				viewBox='-20 -20 40 40'
+				className='w-60 h-60 mx-auto'
+				onMouseMove={handleMouseMove} // Track mouse movement over the SVG
+			>
 				{data.map((slice, index) => {
 					const [startX, startY] = getCoordinatesForPercent(cumulativeValue / total, radius)
 					cumulativeValue += slice.value
@@ -26,11 +37,27 @@ const PieChart = ({ data, setHoveredPieSlice, hoveredPieSlice, cursorPosition, s
 							key={index}
 							d={`M ${centerX} ${centerY} L ${startX} ${startY} A ${radius} ${radius} 0 ${largeArcFlag} 1 ${endX} ${endY} Z`}
 							fill={slice.color}
-							className='transition-transform duration-300 hover:scale-110 '
+							className='transition-transform duration-300 hover:scale-110 cursor-pointer '
 						/>
 					)
 				})}
 			</svg>
+
+			{/* Tooltip - only show if a slice is hovered */}
+			{hoveredPieSlice && (
+				<div
+					className='absolute bg-system-secondary-bg text-system-primary-text text-sm p-2 rounded flex flex-col items-center shadow'
+					style={{
+						left: `${cursorPosition.x -50}px`, // Slight offset from the cursor
+						top: `${cursorPosition.y - 100}px`,
+						whiteSpace: 'nowrap',
+					}}>
+					<p className='text-xl'>{hoveredPieSlice.value}</p>
+					{hoveredPieSlice.label}
+					<div className='tooltip-arrow'></div> {/* Arrow */}
+				</div>
+			)}
+
 			<div className='flex flex-row justify-center items-center mt-2 gap-3 pb-5'>
 				{data.map((slice, index) => {
 					return (
