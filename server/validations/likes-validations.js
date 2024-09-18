996014787e7d02f1fad29bbab04195e1d41b03dd
vedanthp16 <pvedanth2@gmail.com>
2024-09-18 12:34:50 +0530

    likes validations

---
+++ server/validations/likes-validations.js
@@ -0,0 +1,18 @@
+import Joi from "joi"
+const ValidatePostLikes = (req, res, next) => {
+    const Result = Joi.object({
+        Type : Joi.string().valid("Activity", "Comment", "Article").required()
+    }).validate(req.body, { stripUnknown: true, convert: true });
+    if (Result.error) {
+        const message = Result.error.details.map((detail) => detail.message).join(', ');
+        return res.status(400).json(message);
+    }
+    else {
+        req.body = Result.value;
+        return next();
+    }
+}
+
+export {
+    ValidatePostLikes
+}
