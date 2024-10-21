f05663ba9f455cbf838598c3bb3a9d249d2f12ad
Karthik M S <k2g1001@gmail.com>
2024-10-21 17:03:10 +0530

    remove animation

--- horasis-frontend/src/components/Common/EmptyMembers.jsx
+++ horasis-frontend/src/components/Common/EmptyMembers.jsx
@@ -2,7 +2,7 @@ import empty from '../../assets/icons/empty.svg'
 const EmptyMembers = ({ emptyText }) => {
   return (
     <div className='flex flex-col gap-3 items-center justify-center w-full p-10  bg-system-secondary-bg rounded-lg'>
-      <img src={empty} alt='Empty' className='animate-bounce' />
+      <img src={empty} alt='Empty'  />
       <p className='text-brand-gray-dim text-center'>{emptyText}</p>
     </div>
   )
