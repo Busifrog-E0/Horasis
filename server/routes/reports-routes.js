98a501499fe1623221ef1e1b993b22aae3966432
Vedanth <pvedanth2@gmail.com>
2024-12-01 12:41:56 +0530

    access for reports and tags search

--- server/routes/reports-routes.js
+++ server/routes/reports-routes.js
@@ -11,23 +11,23 @@ import e from 'express';
 const router = e.Router();
 router.route
 
-router.get('/reports', decodeIDToken, ensureAuthorized, ValidateGetReports, SwaggerDocs.get_Reports,
+router.get('/reports', decodeIDToken, ensureAuthorized("Admin","SuperAdmin"), ValidateGetReports, SwaggerDocs.get_Reports,
     // @ts-ignore
     asyncHandler(GetReports));
 
-router.get('/reports/:ReportId', decodeIDToken, ensureAuthorized, SwaggerDocs.get_Reports_ReportId,
+router.get('/reports/:ReportId', decodeIDToken, ensureAuthorized("Admin", "SuperAdmin"), SwaggerDocs.get_Reports_ReportId,
     // @ts-ignore
     asyncHandler(GetOneFromReports));
 
-router.post('/reports', decodeIDToken, ensureAuthorized, ValidatePostReports, SwaggerDocs.post_Reports,
+router.post('/reports', decodeIDToken, ensureAuthorized("User","Admin"), ValidatePostReports, SwaggerDocs.post_Reports,
     // @ts-ignore
     asyncHandler(PostReports));
 
-router.patch('/reports/:ReportId', decodeIDToken, ensureAuthorized, ValidatePatchReports, SwaggerDocs.patch_Reports_ReportId,
+router.patch('/reports/:ReportId', decodeIDToken, ensureAuthorized("Admin", "SuperAdmin","User"), ValidatePatchReports, SwaggerDocs.patch_Reports_ReportId,
     // @ts-ignore
     asyncHandler(PatchReports));
 
-router.delete('/reports/:ReportId', decodeIDToken, ensureAuthorized, SwaggerDocs.delete_Reports_ReportId,
+router.delete('/reports/:ReportId', decodeIDToken, ensureAuthorized("Admin", "SuperAdmin","User"), SwaggerDocs.delete_Reports_ReportId,
     // @ts-ignore
     asyncHandler(DeleteReports));
 
