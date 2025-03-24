dfeed76c065cf71b4119fce6a709933d660decf1
Vedanth <pvedanth2@gmail.com>
2025-03-24 10:21:02 +0530

    profile fix

--- server/controllers/files-controller.js
+++ server/controllers/files-controller.js
@@ -33,6 +33,7 @@ const byteArrayToFile = (byteArray, filePath) => {
 
 import { S3 } from "@aws-sdk/client-s3";
 import e from 'express';
+import moment from 'moment';
 //const fs = readFileSync('./signature.jpg')
 const s3 = new S3({
     forcePathStyle: true, // Configures to use subdomain/virtual calling format.
@@ -120,7 +121,7 @@ const PostFilesUsers = async (req, res) => {
     if (req.body.Type) {
         FilePath = FilePath + `/${req.body.Type}/${Math.floor(Math.random() * 1000000000000) + 1}`
     }
-    SaveFileToSpaces("Users", FilePath + `/${req.body.FileFieldName}`, req.body.FileData, FileType, res)
+    SaveFileToSpaces("Users", FilePath + `/${req.body.FileFieldName}${moment().valueOf()}`, req.body.FileData, FileType, res)
 }
 
 
