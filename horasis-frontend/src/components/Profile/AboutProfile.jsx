df8410e3cfb19e813c8962508f586fc1039735a9
Karthik M S <k2g1001@gmail.com>
2025-04-03 12:08:49 +0530

    spell fix

--- horasis-frontend/src/components/Profile/AboutProfile.jsx
+++ horasis-frontend/src/components/Profile/AboutProfile.jsx
@@ -228,7 +228,7 @@ const AboutProfile = ({ user, getUserDetails, isCurrentUser }) => {
 						</div>
 
 						<div>
-							<h1 className='text-system-primary-text font-medium text-lg'>Interets</h1>
+							<h1 className='text-system-primary-text font-medium text-lg'>Interests</h1>
 							{updateFormValue.Interests && updateFormValue.Interests.length > 0 && (
 								<div className='flex gap-4 px-0 pb-4 my-2 flex-wrap'>
 									{updateFormValue.Interests.map((interest) => {
