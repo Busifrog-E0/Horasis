7f38b6d92b7d75c9d6e55f086fc6b1e98668a2a6
Haris Wilson <harismoylan@gmail.com>
2025-05-12 13:00:38 +0530

    testing new email

--- server/controllers/emails-controller.js
+++ server/controllers/emails-controller.js
@@ -15,39 +15,44 @@ const TestUsers = [
  * @returns {Promise<string|true>}
  */
 const SendOTPEmail = async (Email, OTP, Name) => {
-    const htmlbody = `<table cellspacing="0" cellpadding="0" style="background-color: #F4F6F7; border: 1px solid #eee; width: 100%;">
+    const htmlbody = `<div class=""><div class="aHl"></div><div id=":o6" tabindex="-1"></div><div id=":nf" class="ii gt" jslog="20277; u014N:xr6bB; 1:WyIjdGhyZWFkLWY6MTgzMTg5ODQxMDI4MjQwMjUzNCJd; 4:WyIjbXNnLWY6MTgzMTg5ODQxMDI4MjQwMjUzNCIsbnVsbCxudWxsLG51bGwsMSwwLFsxLDAsMF0sNTMsMzIyLG51bGwsbnVsbCxudWxsLG51bGwsbnVsbCwxLG51bGwsbnVsbCxbMF0sbnVsbCxudWxsLG51bGwsbnVsbCxudWxsLG51bGwsMF0."><div id=":ng" class="a3s aiL "><table cellspacing="0" cellpadding="0" style="background-color:#f4f6f7;border:1px solid #eee;width:100%">
     <tbody>
         <tr>
             <td>
-                <div
-                    style="background-color: #fff; border: 1px solid #DEE6E9; border-radius: 10px; box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; margin: auto; max-width: 600px; overflow: hidden; width: 600px;">
+                <div style="background-color:#fff;border:1px solid #dee6e9;border-radius:10px;box-sizing:border-box;font-family:Lato,Helvetica,'Helvetica Neue',Arial,'sans-serif';margin:auto;max-width:600px;overflow:hidden;width:600px">
 
-
-                        <h2 style="color: #fff; font-size: 24px; font-weight: normal; margin: 0;">Horasis</h2>
+                    <div style="background-color:#25586b;padding:40px;text-align:center;background-image:url();background-repeat:no-repeat;background-position:calc(100% - 20px) 20px;background-size:50px">
+                        <h2 style="color:#fff;font-size:24px;font-weight:normal;margin:0">Horasis</h2>
                     </div>
-                        <p style="font-size: 14px; margin: 0; margin-bottom: 25px;">Hi ${Name}</p>
-                        <p style="font-size: 16px; margin: 0; margin-bottom: 35px; line-height: 22px;">
-                            Below is your
+                    <div style="padding:40px 50px;background-image:url();background-repeat:no-repeat;background-position:top;background-size:contain">
+                        <p style="font-size:14px;margin:0;margin-bottom:25px">Hi ${Name}</p>
+                        <p style="font-size:16px;margin:0;margin-bottom:35px;line-height:22px">
+                            Verify you email address. Below is your
                             <strong>One time password:</strong>
                         </p>
-                        <div style="text-align: center;">
-                            <div
-                                style="background-color: #25586B0D; border-radius: 6px; color: #25586B; display: inline-block; font-size: 30px; padding: 20px 30px;">
+                        <div style="text-align:center">
+                            <div style="background-color:#25586b0d;border-radius:6px;color:#25586b;display:inline-block;font-size:30px;padding:20px 30px">
                                 ${OTP}
                             </div>
                         </div>
-                        <div style="display: flex; align-items: center; justify-content: center; margin-top: 15px;">
+                        <div style="display:flex;margin-top:15px">
+                            <div style="background-image:url();background-repeat:no-repeat;background-size:contain;height:14px;width:14px">
+                            </div>
                         </div>
-                        <p style="font-size: 14px; margin: 0; margin: 35px 0;  line-height: 22px;">If you didn't request
+                        <p style="font-size:14px;margin:0;margin:35px 0;line-height:22px">If you didn't request
                             this one time password, ignore the email.</p>
-                        <p style="font-size: 14px; margin: 0; line-height: 22px;">Thank you,</p>
-                        <p style="font-size: 14px; margin: 0; line-height: 22px;">Team Horasis</p>
+                        <p style="font-size:14px;margin:0;margin-bottom:35px;line-height:22px">If you'd like to
+                            know more about Horasis or want to get in touch with us, get in touch with our
+                            customer support team.</p>
+                        <p style="font-size:14px;margin:0;line-height:22px">Thank you,</p>
+                        <p style="font-size:14px;margin:0;line-height:22px">Team Horasis</p>
                     </div>
                 </div>
             </td>
         </tr>
     </tbody>
-</table>`;
+</table><div class="yj6qo"></div><div class="adL">
+</div></div></div><div class="WhmR8e" data-hash="0"></div></div>`;
 
     return EmailAPI(Email, "Verify OTP", htmlbody);
 }
@@ -164,6 +169,9 @@ const ForgotPasswordOTPEmail = async (Email, FullName, OTP) => {
     return EmailAPI(Email, "Forgot Password", htmlbody);
 }
 
+(async () => {
+    await SendOTPEmail("harismoylan@gmail.com", "123456", "qwertyui");
+})();
 
 export {
     SendOTPEmail, SendSpeakerInviteEmail,
