f06e3a83be39803cba84efc42625200a1ee3e365
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-08-25 15:26:32 +0530

    changes

--- horasis-frontend/src/context/Socket/SocketProvider.jsx
+++ horasis-frontend/src/context/Socket/SocketProvider.jsx
@@ -5,7 +5,7 @@ import { useToast } from '../../components/Toast/ToastService'
 import { io } from 'socket.io-client'
 import { useChatPopup } from '../ChatPopup/ChatPopupService'
 
-const URL = 'https://deploy.busifrog.com'
+const URL = 'https://socialfb.horasis.org'
 const SocketProvider = ({ children }) => {
 	// context
 	const { currentUserData } = useContext(AuthContext)
